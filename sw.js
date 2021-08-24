9d4a6b52f53f003eb5cb3943978356555d461fc5
daydaybigking <daydaybigking@sina.com>
2021-08-24 22:22:08 +0800

    添加 activate 时间清楚不再需要的缓存

--- sw.js
+++ sw.js
@@ -1,26 +1,26 @@
-self.importScripts('data/games.js');
+self.importScripts("data/games.js");
 
 // Files to cache
-const cacheName = 'v1';
+const cacheName = "v1";
 const appShellFiles = [
-  '/pwa-playground/',
-  '/pwa-playground/index.html',
-  '/pwa-playground/app.js',
-  '/pwa-playground/style.css',
-  '/pwa-playground/fonts/graduate.eot',
-  '/pwa-playground/fonts/graduate.ttf',
-  '/pwa-playground/fonts/graduate.woff',
-  '/pwa-playground/favicon.ico',
-  '/pwa-playground/img/js13kgames.png',
-  '/pwa-playground/img/bg.png',
-  '/pwa-playground/icons/icon-32.png',
-  '/pwa-playground/icons/icon-64.png',
-  '/pwa-playground/icons/icon-96.png',
-  '/pwa-playground/icons/icon-128.png',
-  '/pwa-playground/icons/icon-168.png',
-  '/pwa-playground/icons/icon-192.png',
-  '/pwa-playground/icons/icon-256.png',
-  '/pwa-playground/icons/icon-512.png',
+  "/pwa-playground/",
+  "/pwa-playground/index.html",
+  "/pwa-playground/app.js",
+  "/pwa-playground/style.css",
+  "/pwa-playground/fonts/graduate.eot",
+  "/pwa-playground/fonts/graduate.ttf",
+  "/pwa-playground/fonts/graduate.woff",
+  "/pwa-playground/favicon.ico",
+  "/pwa-playground/img/js13kgames.png",
+  "/pwa-playground/img/bg.png",
+  "/pwa-playground/icons/icon-32.png",
+  "/pwa-playground/icons/icon-64.png",
+  "/pwa-playground/icons/icon-96.png",
+  "/pwa-playground/icons/icon-128.png",
+  "/pwa-playground/icons/icon-168.png",
+  "/pwa-playground/icons/icon-192.png",
+  "/pwa-playground/icons/icon-256.png",
+  "/pwa-playground/icons/icon-512.png",
 ];
 const gamesImages = [];
 for (let i = 0; i < games.length; i++) {
@@ -29,25 +29,45 @@ for (let i = 0; i < games.length; i++) {
 const contentToCache = appShellFiles.concat(gamesImages);
 
 // Installing Service Worker
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-  e.waitUntil((async () => {
-    const cache = await caches.open(cacheName);
-    console.log('[Service Worker] Caching all: app shell and content');
-    await cache.addAll(contentToCache);
-  })());
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      console.log("[Service Worker] Caching all: app shell and content");
+      await cache.addAll(contentToCache);
+    })()
+  );
 });
 
 // Fetching content using Service Worker
-self.addEventListener('fetch', (e) => {
-  e.respondWith((async () => {
-    const r = await caches.match(e.request);
-    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-    if (r) return r;
-    const response = await fetch(e.request);
-    const cache = await caches.open(cacheName);
-    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-    cache.put(e.request, response.clone());
-    return response;
-  })());
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) return r;
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })()
+  );
+});
+
+// 清楚不再需要的旧缓存
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    (async (e) => {
+      caches.keys().then((keyList) => {
+        Promise.all(
+          keyList.map((key) => {
+            if (key === cacheName) return;
+            caches.delete(key);
+          })
+        );
+      });
+    })()
+  );
 });
